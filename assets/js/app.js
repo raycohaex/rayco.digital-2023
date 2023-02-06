17990e3f16b641dde0436078f044c7043f567e76
Rayco Haex <raycoh1299@gmail.com>
2023-02-06 13:25:50 +0100

    small tweaks, clean ups

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
 import './homepage';
 import './scrolling.js';
 import './menu.js';
-import './mobile';
 
 /* if body contains homepage class, then run homepage.js */
 if (document.body.classList.contains('homepage')) {
